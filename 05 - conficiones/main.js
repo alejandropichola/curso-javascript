0dea05d7e1e589747b706f55fa8c985c5f9ce431
Alejandro <g4alejandro29@gmail.com>
2020-12-19 18:36:21 -0600

    create conditional

---
+++ 05 - conficiones/main.js
@@ -0,0 +1,21 @@
+const cadena = '1';
+
+const numero = 1;
+
+const cadenaLength = 'Soy una cadena mayor a 5 caracteres';
+
+if (cadena === numero) {
+    // console.log('Es igual');
+} else {
+   // console.log('No es igual');
+}
+
+
+const isEqual = cadena === numero ? 'Es igual' : 'No es igual';
+// console.log(isEqual);
+
+if (cadenaLength.length > 5) {
+    console.log('La cadena es mayor a 5 caracteres');
+} else {
+    console.log('La cadena no es mayor a 5 caractes');
+}
